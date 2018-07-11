e4ee4690cc7d580f97fc17c0e52fb7168e17e298
itsouvalas <itsouvalas@engineyard.com>
2018-07-11 17:34:17 +0300

    removed #

--- cookbooks/custom-cron/attributes/default.rb
+++ cookbooks/custom-cron/attributes/default.rb
@@ -1,5 +1,5 @@
 # Add crons here
 default[:custom_crons] = []
 # Example:
-# default[:custom_crons] = [{:name => "test1", :time => "0-59 * * * *", :command => "echo 'test1'", :instance_name => "solo"},
+ default[:custom_crons] = [{:name => "test0", :time => "0-59 * * * *", :command => "echo 'test1'", :instance_name => "solo"},
 #                         {:name => "test2", :time => "10 1 * * *", :command => "echo 'test2'", :instance_name => "cron"}]
