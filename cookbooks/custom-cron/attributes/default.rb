58436a6b87bd371b604e0a61219589e16720cea2
itsouvalas <itsouvalas@engineyard.com>
2018-07-11 19:43:16 +0300

    instance set to all

--- cookbooks/custom-cron/attributes/default.rb
+++ cookbooks/custom-cron/attributes/default.rb
@@ -1,5 +1,5 @@
 # Add crons here
 # default[:custom_crons] = []
 # Example:
-default[:custom_crons] = [{:name => "test0", :time => "* * * * *", :command => "echo 'test1'", :instance_name => "solo"},
+default[:custom_crons] = [{:name => "test0", :time => "* * * * *", :command => "echo 'test1'", :instance_name => "all"},
                          {:name => "test2", :time => "10 1 * * *", :command => "echo 'test2'", :instance_name => "cron"}]
