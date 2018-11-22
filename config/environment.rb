42a2ce44101a17521b443ce6d2142328dfadd8da
itsouvalas <itsouvalas@engineyard.com>
2018-11-22 13:57:05 +0000

    remove le

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-Rails.logger = Le.new('6c3ccc92-939e-4442-8da7-1ce67add497b')
