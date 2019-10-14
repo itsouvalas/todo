2e8053448cbbf67e902c40d8dc88cc626f6d31b1
user <itsouvalas@engineyard.com>
2019-10-14 23:36:17 +0300

    test deploy hook

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -3,7 +3,7 @@ symink= "#{config.release_path}/reports"
 unless File.exists?(path)
 	run "mkdir -p /data/shared/reports"
 end
-​
+
 unless File.exists?(symlink)
 	run "ln -nfs /data/shared/reports #{config.release_path}/reports"
 end
